f34de456b766e42c3c0adf8e71c1b6d5fa9775e1
Dattatrya Moin <dattatryamoin@gmail.com>
2023-07-18 13:05:07 +0530

    Excercies for Connectting SQL Server with EF

---
+++ EF_SQLServer/EF_SQLServer/Models/EF/DeptInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EF_SQLServer.Models.EF;
+
+public partial class DeptInfo
+{
+    public int DeptNo { get; set; }
+
+    public string? DeptName { get; set; }
+
+    public string? DeptLocation { get; set; }
+}
---
+++ EF_SQLServer/EF_SQLServer/Models/EF/TrainingContext.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_SQLServer.Models.EF;
+
+public partial class TrainingContext : DbContext
+{
+    public TrainingContext()
+    {
+    }
+
+    public TrainingContext(DbContextOptions<TrainingContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<DeptInfo> DeptInfos { get; set; }
+
+    public virtual DbSet<EmployeeDetail> EmployeeDetails { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("server=.;database=Training;integrated security=true; TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<DeptInfo>(entity =>
+        {
+            entity.HasKey(e => e.DeptNo).HasName("PK__deptInfo__BE2D3F558EFB2110");
+
+            entity.ToTable("deptInfo");
+
+            entity.Property(e => e.DeptNo)
+                .ValueGeneratedNever()
+                .HasColumnName("deptNo");
+            entity.Property(e => e.DeptLocation)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("deptLocation");
+            entity.Property(e => e.DeptName)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("deptName");
+        });
+
+        modelBuilder.Entity<EmployeeDetail>(entity =>
+        {
+            entity.HasKey(e => e.EmpNo).HasName("PK__employee__AFB335920985C428");
+
+            entity.ToTable("employeeDetails");
+
+            entity.Property(e => e.EmpNo)
+                .ValueGeneratedNever()
+                .HasColumnName("empNo");
+            entity.Property(e => e.EmpDeptno).HasColumnName("empDeptno");
+            entity.Property(e => e.EmpDesignation)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("empDesignation");
+            entity.Property(e => e.EmpIsPermenant).HasColumnName("empIsPermenant");
+            entity.Property(e => e.EmpName)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("empName");
+            entity.Property(e => e.EmpSalary).HasColumnName("empSalary");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
